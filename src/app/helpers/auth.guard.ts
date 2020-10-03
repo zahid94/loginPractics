27ed75374bf429e47895f09e5e754e0231b8e979
zahidul islam <zahidulislam5594@gmail.com>
2020-10-03 21:17:29 +0600

    add guard and error interceptor

--- src/app/helpers/auth.guard.ts
+++ src/app/helpers/auth.guard.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthenticationService) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (this.authService.currentUserValue) { return true; }
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
   }
-  
 }
--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -5,14 +5,24 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
+import { catchError } from 'rxjs/operators';
+import { Location } from '@angular/common';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService:AuthenticationService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+    .pipe(catchError(error=>{
+      if(error.status===401){
+        this.authService.logout();
+        location.reload(true);
+      }
+       return throwError(error.error.message || error.statusText);
+    }));
   }
 }
